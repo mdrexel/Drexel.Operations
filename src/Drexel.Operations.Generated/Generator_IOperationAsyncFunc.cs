dc56b199ab76fd7121fbf2630c5f1d04f212677e
mdrexel <max-github@drexel.dev>
2022-02-04 01:25:49 -0800

    Leave a half-started generator so I remember where I left off next time

---
+++ src/Drexel.Operations.Generated/Generator_IOperationAsyncFunc.cs
@@ -0,0 +1,10 @@
+﻿namespace Drexel.Operations.Generated
+{
+    public sealed class Generator_IOperationAsyncFunc : GeneratorBase
+    {
+        public Generator_IOperationAsyncFunc(uint order)
+            : base(order)
+        {
+        }
+    }
+}
